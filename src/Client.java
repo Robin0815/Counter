268506cbd933667210f564eaac496760d4784a78
Robin0815 <robin_frank_ccaa@yahoo.de>
2020-12-09 23:18:23 +0100

    Logger impl

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,16 @@
+import org.hbrs.ooka.ws2020.uebung2.logger.Inject;
+import org.hbrs.ooka.ws2020.uebung2.logger.LoggerInterface;
+
 public class Client {
     private static int counter = 0;
     private static GUI gui;
+
+    @Inject
+    public static LoggerInterface myLog;
+
     @Start
     public static void main(String[] args) {
+        myLog.sendLog("Started");
         Controller con = new Controller();
         gui = new GUI("Counter"+counter);
         counter++;
